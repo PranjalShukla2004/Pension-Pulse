4c47b76e58d4f5700f3a0ddd188fc3a509bfb840
PranjalShukla2004 <s2427231@ed.ac.uk>
2025-03-02 10:31:43 +0000

    L

--- trainer.py
+++ trainer.py
@@ -1,8 +1,7 @@
-from models import train_agents, save_model_predictions_to_db
+from models import train_agents
 import config
 
 
 agents, _ = train_agents(episodes=config.EPISODES)
 for i, agent in enumerate(agents):
     db_path = config.PREDICTIONS_DB_PATH_TEMPLATE.format(bank_id=i+1)
-    save_model_predictions_to_db(agent, db_path=db_path)
